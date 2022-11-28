f0cb9eb5e927645760245da852a74e92e793a61f
Лилия <l_loshakova123@mail.ru>
2022-11-28 21:42:08 +0300

    Solution task

--- Task2.java
+++ Task2.java
@@ -1,8 +1,8 @@
 // 1:32:50
 package Seminar1;
-
 import java.time.LocalDateTime;
 import java.util.Scanner;
+
 
 public class Task2 {
     public static void main(String[] args) {
@@ -12,15 +12,14 @@ public class Task2 {
 // Считываем строку с консоли
         String name = iScanner.nextLine();
 // Форматированный вывод
-
         int hour = LocalDateTime.now().getHour();
         String text = "";
        
-                if ((hour >= 5) && (hour < 12)) text= "Доброе утро, ";
-                else if ((hour >= 12) && (hour < 18)) text= "Добрый день, ";
-                else if ((hour >= 18) && (hour < 23)) text= "Добрый вечер, ";
-                else text= "Доброй ночи, ";
 
+        if ((hour >= 5) && (hour < 12)) text= "Доброе утро, ";
+        else if ((hour >= 12) && (hour < 18)) text= "Добрый день, ";
+        else if ((hour >= 18) && (hour < 23)) text= "Добрый вечер, ";
+        else text= "Доброй ночи, ";
         
 
         System.out.printf("%s %s", text, name);
