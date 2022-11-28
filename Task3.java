fe63578e35396b255ba2d350dce1dcb75a6f7d24
Лилия <l_loshakova123@mail.ru>
2022-11-28 21:53:37 +0300

    New file

---
+++ Task3.java
@@ -0,0 +1,22 @@
+package Seminar1;
+
+public class Task3 {
+    public static void main(String[] args) {
+
+        int[] arr = new int[] { 1, 1, 0, 1, 1, 1 };
+        int count = 0;
+        int max = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if ((arr[i] == 1) && (arr[i - 1] == 1))
+                count++;
+            else {
+                if (count > max) {
+                    max = count;
+                    count = 0;
+                }
+            }
+        }
+
+    }
+
+}
