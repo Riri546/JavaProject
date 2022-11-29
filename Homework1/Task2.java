1b753d728613cbcc1453b639876c61b22a701a1f
Лилия <l_loshakova123@mail.ru>
2022-11-29 19:19:52 +0300

    Solution task

--- Homework1/Task2.java
+++ Homework1/Task2.java
@@ -1,26 +1,28 @@
 package Seminar1.Homework1;
 
-// Вывести все простые числа от 1 до 1000
+// 1. Вывести все простые числа от 1 до 1000
 
 public class Task2 {
     public static void main(String[] args) {
-        int count = 0;
-        // Цикл собирает все числа от 1 до 1000. Для того, что бы на консоль вывелась 1000, а не 999, указываю 1001
+        // Цикл собирает все числа от 1 до 1000. Для того, что бы на консоль вывелась
+        // 1000, а не 999, указываю 1001
         for (int i = 1; i < 1001; i++) {
-                // System.out.println(i);
-                if ((i % 1) == 0){
-                    count = i;
-                    System.out.println(count);
-                }
-                else if ((i % i) == 0){
-                    count = i;
-                    System.out.println(count);
-                }
-                else{
-                    i++;
-                }
+            // Условие для запуска работы функции и вывода ответа на печать
+            if (checkSimpleNumber(i) == true) {
+                System.out.println(i);
             }
         }
-        
     }
 
+    // Функция для поиска простых чисел
+    public static Boolean checkSimpleNumber(int number) {
+        // цикл перебирает все числа от 1 до 1000
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            // Условие проверяет является ли число простым
+            if ((number == 1) || (number % i == 0)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
