14db91172ae35d38dae787df275458e857c2f3ac
Лилия <l_loshakova123@mail.ru>
2022-11-29 20:13:23 +0300

    Solution task

--- Homework1/Task3.java
+++ Homework1/Task3.java
@@ -6,58 +6,57 @@ import java.util.Scanner;
 
 public class Task3 {
     public static void main(String[] args) {
-        
+
         System.out.println("Привет! Ты  попал в простой, классический калькулятор)))");
         Scanner scanner = new Scanner(System.in);
         // Ввод данных от пользователя
-        System.out.println("Введи первой число: ");
-        //Первое число
+        System.out.print("Введи первой число: ");
+        // Первое число
         int a = scanner.nextInt();
-        System.out.println("Какую операцию нужно произвести? Выбери оператор (+ - * /): ");
-        //Оператор
+        System.out.print("Какую операцию нужно произвести? Выбери оператор (+ - * /): ");
+        // Оператор
         char op = scanner.next().charAt(0);
-        System.out.println("Введи второе число: ");
+        System.out.print("Введи второе число: ");
         // Второе число
         int b = scanner.nextInt();
-        
-            if (op == '+'){
-                System.out.println(a + " + " + b + " = " + add(a, b));
-            } 
-            else if (op == '-'){
-                System.out.println(a + " - " + b + " = " + minus(a, b));
-            }   
-    
-            case '-':
-                System.out.println(a + " - " + b + " = " + minus(a, b));
-                break;
-            case '*':
-                System.out.println(a + " * " + b + " = " + mult(a, b));
-                break;
-            case '/':
-                System.out.println(a + " / " + b + " = " + divide(a, b));
-                break;
-            default:
-                System.out.println("Wrong operation!");
+
+        // Проверка на то какой оператор и вывод решения
+        if (op == '+') {
+            System.out.println(a + " + " + b + " = " + add(a, b));
+        } else if (op == '-') {
+            System.out.println(a + " - " + b + " = " + minus(a, b));
+        } else if (op == '*') {
+            System.out.println(a + " * " + b + " = " + mult(a, b));
+        } else if (op == '/') {
+            System.out.println(a + " / " + b + " = " + divide(a, b));
+        } else {
+            System.out.println("Не верно введен оператор(((");
         }
         scanner.close();
     }
 
-    private static int divide(int a, int b) {
-        if (b != 0)
-            return a / b;
-        else
-            return -1;
+    // Функция для сложения
+    private static int add(int a, int b) {
+        return a + b;
     }
 
-    private static int mult(int a, int b) {
-        return a * b;
-    }
-
+    // Функция для вычетания
     private static int minus(int a, int b) {
         return a - b;
     }
 
-    private static int add(int a, int b) {
-        return a + b;
+    // Функция для умножения
+    private static int mult(int a, int b) {
+        return a * b;
+    }
+
+    // Функция для деления
+    private static int divide(int a, int b) {
+        // Проверка на ноль
+        if ((b != 0) && (a != 0))
+            return a / b;
+        else
+            System.out.println("На ноль нельзя делить! Я конечно выведу ноль, но запомни уже это правило!");
+        return 0;
     }
 }
