5398c9638beea08ed815160656cf4688104e972a
Лилия <l_loshakova123@mail.ru>
2022-12-14 21:34:31 +0300

    Outline of the solution

---
+++ Seminar2/Workhome2/Task4.java
@@ -0,0 +1,61 @@
+package Seminar2.Workhome2;
+import java.util.Scanner;
+
+// 3. Реализовать простой калькулятор + - / * 
+public class Task4 {
+    
+    public static void main(String[] args) {
+
+        System.out.println("Привет! Ты  попал в простой, классический калькулятор)))");
+        Scanner scanner = new Scanner(System.in);
+        // Ввод данных от пользователя
+        System.out.print("Введи первой число: ");
+        // Первое число
+        int a = scanner.nextInt();
+        System.out.print("Какую операцию нужно произвести? Выбери оператор (+ - * /): ");
+        // Оператор
+        char op = scanner.next().charAt(0);
+        System.out.print("Введи второе число: ");
+        // Второе число
+        int b = scanner.nextInt();
+
+        // Проверка на то какой оператор и вывод решения
+        if (op == '+') {
+            System.out.println(a + " + " + b + " = " + add(a, b));
+        } else if (op == '-') {
+            System.out.println(a + " - " + b + " = " + minus(a, b));
+        } else if (op == '*') {
+            System.out.println(a + " * " + b + " = " + mult(a, b));
+        } else if (op == '/') {
+            System.out.println(a + " / " + b + " = " + divide(a, b));
+        } else {
+            System.out.println("Не верно введен оператор(((");
+        }
+        scanner.close();
+    }
+
+    // Функция для сложения
+    private static int add(int a, int b) {
+        return a + b;
+    }
+
+    // Функция для вычетания
+    private static int minus(int a, int b) {
+        return a - b;
+    }
+
+    // Функция для умножения
+    private static int mult(int a, int b) {
+        return a * b;
+    }
+
+    // Функция для деления
+    private static int divide(int a, int b) {
+        // Проверка на ноль
+        if ((b != 0) && (a != 0))
+            return a / b;
+        else
+            System.out.println("На ноль нельзя делить! Я конечно выведу ноль, но запомни уже это правило!");
+        return 0;
+    }
+}
