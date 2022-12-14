2c67152e76fde03b03b813f2e8b3f2efdda208e8
Лилия <l_loshakova123@mail.ru>
2022-12-14 22:08:54 +0300

    Outline of the solution

--- Seminar2/Workhome2/Task4.java
+++ Seminar2/Workhome2/Task4.java
@@ -1,10 +1,11 @@
 package Seminar2.Workhome2;
+
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.sql.Timestamp;
-import java.util.Arrays;
+
 
 // 3. Реализовать простой калькулятор + - / * 
 public class Task4 {
@@ -12,16 +13,16 @@ public class Task4 {
     private static File log;
     private static FileWriter fileWriter;
 
-    //Метод соания файла для логирования
+    // Метод соания файла для логирования
     public static void main(String[] args) {
         try {
-//Создаем файл
+            // Создаем файл
             log = new File("calculatorLog.txt");
             log.createNewFile();
             fileWriter = new FileWriter(log);
-//Заводим данные для сортировки 
-            model(args);
-// Делаем проверку на сбои, что бы не возникало ошибок 
+            // Заводим данные для сортировки
+            // model(arg);
+            // Делаем проверку на сбои, что бы не возникало ошибок
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
@@ -32,8 +33,8 @@ public class Task4 {
             }
         }
     }
-    
-    public static void model(String[] args) {
+
+    public static void model(String[] arg) {
 
         System.out.println("Привет! Ты  попал в простой, классический калькулятор)))");
         Scanner scanner = new Scanner(System.in);
@@ -47,20 +48,26 @@ public class Task4 {
         System.out.print("Введи второе число: ");
         // Второе число
         int b = scanner.nextInt();
-
+        int res;
+        
         // Проверка на то какой оператор и вывод решения
         if (op == '+') {
-            System.out.println(a + " + " + b + " = " + add(a, b));
+            res = add(a, b);
+            System.out.println(a + " + " + b + " = " + res);
         } else if (op == '-') {
-            System.out.println(a + " - " + b + " = " + minus(a, b));
+            res = minus(a, b);
+            System.out.println(a + " - " + b + " = " + res);
         } else if (op == '*') {
-            System.out.println(a + " * " + b + " = " + mult(a, b));
+            res = mult(a, b);
+            System.out.println(a + " * " + b + " = " + res);
         } else if (op == '/') {
-            System.out.println(a + " / " + b + " = " + divide(a, b));
+            res = divide(a, b);
+            System.out.println(a + " / " + b + " = " + res);
         } else {
             System.out.println("Не верно введен оператор(((");
         }
         scanner.close();
+        
     }
 
     // Функция для сложения
@@ -86,5 +93,14 @@ public class Task4 {
         else
             System.out.println("На ноль нельзя делить! Я конечно выведу ноль, но запомни уже это правило!");
         return 0;
+    }
+
+    // Метод для записи результатов в файл логирования
+    public static void logStep(int note) {
+        try {
+            fileWriter.write(note);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
