e7cd9ba0272fd9d80f259c6c17708359a19af841
Лилия <l_loshakova123@mail.ru>
2022-12-23 18:45:40 +0300

    Solurion task

--- Seminar4/Homework4/Task1.java
+++ Seminar4/Homework4/Task1.java
@@ -6,15 +6,18 @@ import java.util.LinkedList;
 
 public class Task1 {
     public static void main(String[] args) {
-
-        int[] listMas = { 5, 3, 2, 6, 7, 4, 9, 1 };
+        //Задаем массив 
+        int[] listMas = { 15, 55, 98, 24, 25, 1 };
+        //И дополнительно создаем пустой массив 
         LinkedList<Integer> linkList = new LinkedList<>();
 
+        //Заполняем пустой массив 
         for (Integer temp : listMas) {
             linkList.add(temp);
         }
 
         int temp = 0;
+        //Заполняем массив в обратном порядке 
         while (temp != linkList.size() - 1) {
             linkList.add(linkList.size() - temp, linkList.getFirst());
             linkList.removeFirst();
