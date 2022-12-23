f85bbaf55996b6a2e9610919317e038aea25c051
Лилия <l_loshakova123@mail.ru>
2022-12-23 18:50:43 +0300

    Outline of the solution

---
+++ Seminar4/Homework4/Task2.java
@@ -0,0 +1,48 @@
+// 2. Реализуйте очередь с помощью LinkedList со следующими методами:
+// enqueue() - помещает элемент в конец очереди, 
+// dequeue() - возвращает первый элемент из очереди и удаляет его, 
+// first() - возвращает первый элемент из очереди, не удаляя.
+
+package Seminar4.Homework4;
+
+import java.util.LinkedList;
+
+public class Task2 {
+    public static void main(String[] args) {
+        //Задаем массив
+        int[] list = { 1, 5, 4, 46, 9, 7, 8 ,74 ,8 };
+        //Создаем массив, класса linkList и заполняем исходным
+        LinkedList<Integer> linkList = new LinkedList<>();
+        for (Integer temp : list) {
+            linkList.add(temp);
+        }
+
+        System.out.println(linkList);
+
+        enqueue(linkList, 9);
+        System.out.println(linkList);
+
+        System.out.println(dequeue(linkList));
+        System.out.println(linkList);
+
+        System.out.println(first(linkList));
+    }
+
+    public static void enqueue(LinkedList<Integer> list, int num) {
+        list.addLast(num);
+    }
+
+    public static int dequeue(LinkedList<Integer> list) {
+        int num = 0;
+        num = list.get(0);
+        list.remove(0);
+        return num;
+    }
+
+    public static int first(LinkedList<Integer> list) {
+        int num = 0;
+        num = list.get(0);
+        return num;
+    }
+}
+
