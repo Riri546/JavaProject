f4763e5158f77478335b05e75d8b998bcc78a11a
Лилия <l_loshakova123@mail.ru>
2022-12-27 11:40:35 +0300

    Outline of the solution

--- Seminar5/Homework/Task1.java
+++ Seminar5/Homework/Task1.java
@@ -3,10 +3,9 @@
 
 package Seminar5.Homework;
 
-import java.util.Scanner;
+// import java.util.Scanner;
 import java.util.HashMap;
 import java.util.List;
-
 
 public class Task1 {
 
@@ -17,18 +16,23 @@ public class Task1 {
     // System.out.println("Введите фамилию с большой буквы и я покажу вам номера телефонов этого абонента");
     // String sname = scanner.next();
         addInPhoneBook();
-        // findInPhoneBook("Иванов");
+        // findInPhoneBook(sname);
+        findInPhoneBook("Колесникова");
         // scanner.close();
     }
 
     public static void addInPhoneBook() {
-        phoneBook.put("Иванов", List.of("+7(111)111-11-11", "+7(222)222-22-22"));
+        phoneBook.put("Иванов", List.of("+7(111)111-11-11", "+7(666)666-66-66"));
+        phoneBook.put("Карпова", List.of("+7(555)555-55-55", "+7(666)666-66-66"));
         phoneBook.put("Петров", List.of("+7(333)333-33-33", "+7(444)444-44-44"));
-        phoneBook.put("Сидоров", List.of("+7(555)555-55-55", "+7(666)666-66-66"));
+        phoneBook.put("Колесникова", List.of("+7(777)555-25-66", "+7(666)666-66-66"));
+        phoneBook.put("Сидоров", List.of("+7(599)556-56-55", "+7(666)666-66-66"));
+        phoneBook.put("Берестова", List.of("+7(555)555-55-55", "+7(666)666-66-66"));
+        phoneBook.put("Косова", List.of("+7(555)555-55-55", "+7(666)666-66-66"));
 
     }
 
     public static void findInPhoneBook(String surname) {
-        System.out.printf("%s: %s", surname, phoneBook.get(surname));
+        System.out.print(surname + ":" + phoneBook.get(surname));
     }
 }
